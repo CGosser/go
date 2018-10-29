b81e674406fe4349d30b89d88031b355a8514e53
Chris Gosser and Joe Friesen <chris+joe@>
2018-10-29 14:34:28 -0700

    Add method Game.prototype.buildGroup. Rename 'Game.board' to 'Game.gameState'.

--- src/models/game.model.ts
+++ src/models/game.model.ts
@@ -4,7 +4,7 @@ export class Game {
   dimension: number;
   whitePlayer: Player;
   blackPlayer: Player;
-  board: any[];
+  gameState: any[];
   activeGame: boolean;
   currentTurn: Player; // either this.whitePlayer or this.blackPlayer;
   lastTurnPass: boolean;
@@ -15,7 +15,7 @@ export class Game {
     this.dimension = dim;
     this.whitePlayer = white;
     this.blackPlayer = black;
-    this.board = createBoard(dim);
+    this.gameState = createNewGameState(dim);
     this.active = true;
     this.currentTurn = this.blackPlayer;
     this.lastTurnPass = false;
@@ -23,7 +23,7 @@ export class Game {
     this.blackCaptures = 0;
   }
 
-  createBoard(N: number) {
+  createNewGameState(N: number) {
     const out: any[] = [];
     for (let i = 0; i < N; i++) {
       const row: any[] = [];
@@ -35,11 +35,11 @@ export class Game {
     return out;
   }
 
-  checkPoint(public point: number[]) {  // point: number is an element of {9, 13, 19}
-    return this.board[point[0]][point[1]]
+  checkPoint(public point: number[]) {  // point: number is an element of list {9, 13, 19}
+    return this.gameState[point[0]][point[1]];
   }
 
-  checkAdjacencies(public point: number[]) { // given a single point, returns a list of points in the board adjacent to that point
+  checkAdjacencies(public point: number[]) { // given a single point, returns a list of points in the gameState adjacent to that point
     const points: any[] = [];
     const i: number = point[0];
     const j: number = point[1];
@@ -55,7 +55,46 @@ export class Game {
     if (j > 0) { // Add the point directly LEFT of the given point to adjacencies list
       points.push([i, j-1]);
     }
-    return points
+    return points;
+  }
+
+  buildGroup(public point: number[]) {
+    // initialization
+    const queue = [point];
+    const group = [point];
+    const liberties: any[] = [];
+    const visited: any[] = [];
+    const i = point[0];
+    const j = point[1];
+    const color = this.gameState[i][j];
+
+    // search
+    while (queue.length > 0) {
+      const next = queue.shift();
+      const neighbors = checkAdjacencies(next);
+      neighbors.forEach(neighbor => {
+        // check if neighbor has been searched. If yes, skip it.
+        let skip = false;
+        visited.forEach(visitedPoint => {
+          if (neighbor[0] === visitedPoint[0] && neighbor[1] === visitedPoint[1]) { skip = true; }
+        })
+        if (skip == false) { // neighbor has not been searched
+          if (this.gameState[neighbor[0]][neighbor[1]] == null) { // neighbor is a liberty
+            liberties.push(neighbor);
+            visited.push(neighbor);
+          }
+          else if (this.gameState[neighbor[0]][neighbor[1]] == color) { // neighbor is in the same group as point
+            group.push(neighbor);
+            queue.push(neighbor);
+          }
+          visited.push(neighbor);
+        }
+      })
+    }
+
+    return {
+      liberties: liberties, // list of all empty points adjacent to members of the group. If the group is dead, liberties = [].
+      group: group}; // list of points in the group
   }
 
 
---
+++ src/models/mock-game-states.ts
@@ -0,0 +1,14 @@
+export gameStates = {
+  var board1 = [
+    [null,    "black", "white",  null,   null, null, null, null, null],
+    ["black", "black", "white", "black", null, null, null, null, null],
+    ["white", "white", "white", "black", null, null, null, null, null],
+    [null,    null,    "white", "white", null, null, null, null, null],
+    [null,    null,    null,    null,    null, null, null, null, null],
+    [null,    null,    null,    null,    null, null, null, null, null],
+    [null,    null,    null,    null,    null, null, null, null, null],
+    [null,    null,    null,    null,    "white", "black", null, null, null],
+    [null,    null,    null,    null,    "white", null, null, null, null]
+  ]
+
+}
