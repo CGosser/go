fa05af18415d592bd9739f80a9470e1cba156ded
Ben Kiggen and Michael Harmon <ben@>
2018-10-29 16:34:48 -0700

    9X9 board intact

--- src/app/board-ui/board-ui.component.ts
+++ src/app/board-ui/board-ui.component.ts
@@ -9,10 +9,12 @@ export class BoardUIComponent implements OnInit {
   size: number = 9;
   rows: number[] = Array(this.size);
   columns: number[] = Array(this.size);
+  dotPosition = [[3,3],[3,7],[7,3],[7,7],[5,5]]
+
   gameState = [["black", "black", null, null, null, null, null, null, null],
                [null, null, null, null, null, null, null, null, null],
                [null, "black", null, null, null, null, null, null, null],
-               [null, null, null, null, null, null, null, null, null],
+               [null, null, null, null, null, null, "black", null, null],
                [null, null, null, "white", null, null, null, null, null],
                [null, null, null, null, null, null, null, null, null],
                [null, null, null, null, null, null, "black", null, null],
