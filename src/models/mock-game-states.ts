e5a5ff385ff314cb101255cd3a21955ce0e6c061
Chris Gosser and Joe Friesen <chris@>
2018-10-30 16:19:01 -0700

    Add more tests to app.component.spec.ts, need to chase down overcounting in Game.resolveScore().

---
+++ src/models/mock-game-states.ts
@@ -0,0 +1,11 @@
+game.gameState = [
+  ["white",null,"white","white","white","black",null,"black","white"],
+  [null,"white","white","white","black","black",null,"black","white"],
+  ["white","white","black","black","black","black","black","black","white"],
+  ["white","black","black","black","white","black","white","white","white"],
+  ["white","black","black","white","white","black","white",null,null],
+  ["black","black","black","black","white","white",null,"white",null],
+  ["black",null,"black","black","white","white",null,"white",null],
+  ["black","black","white","white","white",null,null,"white",null],
+  ["white","white","white","white",null,null,null,"white",null]
+];
